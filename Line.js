09d6c673f0bf22239aa9e13a648e4f4c3df66662
Huajian Mao <huajianmao@gmail.com>
2019-08-08 23:04:26 +0800

    Use PropTypes from 'prop-types' package

--- Line.js
+++ Line.js
@@ -1,11 +1,11 @@
 import React, {
-    PropTypes,
     Component,
 } from 'react'
 import {
     StyleSheet,
     View,
 } from 'react-native'
+import PropTypes from 'prop-types'
 
 import * as Utils from './Utils'
 
